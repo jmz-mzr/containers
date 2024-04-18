612a04e31e2a8bc9b1207d02799f410c761a27f4
jmazoyer <jmz.mazoyer@gmail.com>
2024-04-18 13:47:45 +0200

    💬 Add a "bright white" text color

--- includes/colors.hpp
+++ includes/colors.hpp
@@ -15,6 +15,7 @@
 
 # define B_CYAN		"\033[0;96m"
 # define B_GREEN	"\033[0;92m"
+# define B_WHITE    "\033[1;37m"
 # define RED		"\033[0;31m"
 # define GRAY		"\033[0;90m"
 # define NO_COLOR	"\033[m"
